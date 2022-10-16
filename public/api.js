1bc917a551033e98f5915d784a82302513ef96d7
kotborealis <kotborealis@awooo.ru>
2022-10-16 15:43:25 +0300

    fix path resolution in demo

--- public/api.js
+++ public/api.js
@@ -1,9 +1,9 @@
-export const basePath = `/sandbox`;
+export const pathname = window.location.pathname.split('/').filter(id => id).slice(0,-1).join('/');
 export const host = window.location.host;
 export const protocolHttp = window.location.protocol;
 export const protocolWs = window.location.protocol === `https:` ? `wss:` : `ws:`;
 
-export const httpApiUrl = `${protocolHttp}//${host}${basePath}`;
-export const wsApiUrl = `${protocolWs}//${host}${basePath}`;
+export const httpApiUrl = `${protocolHttp}//${host}${pathname}`;
+export const wsApiUrl = `${protocolWs}//${host}${pathname}`;
 
 console.log({httpApiUrl, wsApiUrl})
